18c274c86675406d2958f523009e67eb400e8559
luke <lrpayne13@gmail.com>
2024-05-23 13:24:40 -0500

    modified:   interactive_dashboard.py

--- 5-11/5-11/interactive_dashboard.py
+++ 5-11/5-11/interactive_dashboard.py
@@ -5,8 +5,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
 import plotly.express as px
-import pandas as pd
-import os
+import os 
 
 # Load data
 df_pages = pd.read_csv('./Pages.csv')
@@ -58,17 +57,24 @@ def create_country_choropleth():
                         title='Clicks by Country')
     return fig
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
 app.layout = html.Div([
-    html.H1('Loud Interactive SEO Dashboard'),
+    html.H1('Interactive Dashboard'),
 
     dcc.Tabs([
         dcc.Tab(label='Clicks Over Time', children=[
-            dcc.Store(id='clicks-store', data=True),
-            dcc.Store(id='impressions-store', data=True),
-            dcc.Store(id='ctr-store', data=True),
-            dcc.Store(id='position-store', data=True),
+            dcc.Checklist(
+                id='metric-selector',
+                options=[
+                    {'label': 'Clicks', 'value': 'Clicks'},
+                    {'label': 'Impressions', 'value': 'Impressions'},
+                    {'label': 'CTR', 'value': 'CTR'},
+                    {'label': 'Position', 'value': 'Position'}
+                ],
+                value=['Clicks', 'Impressions'],
+                inline=True
+            ),
             dcc.Graph(id='line-graph')
         ]),
         dcc.Tab(label='CTR vs Position (Pages)', children=[
@@ -137,12 +143,12 @@ app.layout = html.Div([
         ]),
         dcc.Tab(label='Metric by Device', children=[
             dcc.Dropdown(
-                id='metric-selector',
+                id='metric-selector-device',
                 options=[{'label': metric, 'value': metric} for metric in ['Clicks', 'Impressions', 'CTR', 'Position']],
                 value='CTR',
                 clearable=False
             ),
-            dcc.Graph(id='bar-plot')
+            dcc.Graph(id='bar-plot-device')
         ]),
         dcc.Tab(label='Search Appearance', children=[
             dcc.Graph(id='search-appearance-bar', figure=create_search_appearance_bar())
@@ -155,37 +161,71 @@ app.layout = html.Div([
 
 @app.callback(
     Output('line-graph', 'figure'),
-    [Input('clicks-store', 'data'),
-     Input('impressions-store', 'data'),
-     Input('ctr-store', 'data'),
-     Input('position-store', 'data')]
+    [Input('metric-selector', 'value')]
 )
-def update_line_graph(show_clicks, show_impressions, show_ctr, show_position):
+def update_line_graph(selected_metrics):
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-    if show_clicks:
+    yaxis1_label = yaxis2_label = None
+
+    if 'Clicks' in selected_metrics:
         fig.add_trace(
-            go.Scatter(x=df_dates['Date'], y=df_dates['Clicks'], mode='lines+markers', name='Clicks', hovertemplate="Clicks: %{y}<extra></extra>"),
+            go.Scatter(x=df_dates['Date'], y=df_dates['Clicks'], mode='lines+markers', name='Clicks',
+                       hovertemplate="Date: %{x}<br>Clicks: %{y}<extra></extra>"),
             secondary_y=False,
         )
-    if show_impressions:
-        fig.add_trace(
-            go.Scatter(x=df_dates['Date'], y=df_dates['Impressions'], mode='lines+markers', name='Impressions', hovertemplate="Impressions: %{y}<extra></extra>"),
-            secondary_y=False,
-        )
-    if show_ctr:
-        fig.add_trace(
-            go.Scatter(x=df_dates['Date'], y=df_dates['CTR'], mode='lines+markers', name='CTR', hovertemplate="CTR: %{y:.2%}<extra></extra>"),
-            secondary_y=True,
-        )
-    if show_position:
-        fig.add_trace(
-            go.Scatter(x=df_dates['Date'], y=df_dates['Position'], mode='lines+markers', name='Position', hovertemplate="Position: %{y}<extra></extra>"),
-            secondary_y=True,
-        )
+        yaxis1_label = "Clicks"
+
+    if 'Impressions' in selected_metrics:
+        if yaxis1_label:
+            fig.add_trace(
+                go.Scatter(x=df_dates['Date'], y=df_dates['Impressions'], mode='lines+markers', name='Impressions',
+                           hovertemplate="Date: %{x}<br>Impressions: %{y}<extra></extra>"),
+                secondary_y=True,
+            )
+            yaxis2_label = "Impressions"
+        else:
+            fig.add_trace(
+                go.Scatter(x=df_dates['Date'], y=df_dates['Impressions'], mode='lines+markers', name='Impressions',
+                           hovertemplate="Date: %{x}<br>Impressions: %{y}<extra></extra>"),
+                secondary_y=False,
+            )
+            yaxis1_label = "Impressions"
+
+    if 'CTR' in selected_metrics:
+        if yaxis1_label:
+            fig.add_trace(
+                go.Scatter(x=df_dates['Date'], y=df_dates['CTR'], mode='lines+markers', name='CTR',
+                           hovertemplate="Date: %{x}<br>CTR: %{y:.2%}<extra></extra>"),
+                secondary_y=True,
+            )
+            yaxis2_label = "CTR"
+        else:
+            fig.add_trace(
+                go.Scatter(x=df_dates['Date'], y=df_dates['CTR'], mode='lines+markers', name='CTR',
+                           hovertemplate="Date: %{x}<br>CTR: %{y:.2%}<extra></extra>"),
+                secondary_y=False,
+            )
+            yaxis1_label = "CTR"
+
+    if 'Position' in selected_metrics:
+        if yaxis1_label:
+            fig.add_trace(
+                go.Scatter(x=df_dates['Date'], y=df_dates['Position'], mode='lines+markers', name='Position',
+                           hovertemplate="Date: %{x}<br>Position: %{y}<extra></extra>"),
+                secondary_y=True,
+            )
+            yaxis2_label = "Position"
+        else:
+            fig.add_trace(
+                go.Scatter(x=df_dates['Date'], y=df_dates['Position'], mode='lines+markers', name='Position',
+                           hovertemplate="Date: %{x}<br>Position: %{y}<extra></extra>"),
+                secondary_y=False,
+            )
+            yaxis1_label = "Position"
 
     fig.update_layout(
-        title_text="Clicks Over Time",
+        title_text="Metrics Over Time",
         hovermode='x unified',
         hoverlabel=dict(
             bgcolor="white",
@@ -194,50 +234,12 @@ def update_line_graph(show_clicks, show_impressions, show_ctr, show_position):
         )
     )
 
-    fig.update_yaxes(title_text="Clicks and Impressions", secondary_y=False)
-    fig.update_yaxes(title_text="CTR and Position", secondary_y=True)
+    if yaxis1_label:
+        fig.update_yaxes(title_text=yaxis1_label, secondary_y=False)
+    if yaxis2_label:
+        fig.update_yaxes(title_text=yaxis2_label, secondary_y=True)
 
     return fig
-
-@app.callback(
-    Output('clicks-store', 'data'),
-    [Input('line-graph', 'relayoutData')],
-    [State('clicks-store', 'data')]
-)
-def toggle_clicks_visibility(relayoutData, show_clicks):
-    if relayoutData and 'clicks' in relayoutData:
-        return not show_clicks
-    return show_clicks
-
-@app.callback(
-    Output('impressions-store', 'data'),
-    [Input('line-graph', 'relayoutData')],
-    [State('impressions-store', 'data')]
-)
-def toggle_impressions_visibility(relayoutData, show_impressions):
-    if relayoutData and 'impressions' in relayoutData:
-        return not show_impressions
-    return show_impressions
-
-@app.callback(
-    Output('ctr-store', 'data'),
-    [Input('line-graph', 'relayoutData')],
-    [State('ctr-store', 'data')]
-)
-def toggle_ctr_visibility(relayoutData, show_ctr):
-    if relayoutData and 'ctr' in relayoutData:
-        return not show_ctr
-    return show_ctr
-
-@app.callback(
-    Output('position-store', 'data'),
-    [Input('line-graph', 'relayoutData')],
-    [State('position-store', 'data')]
-)
-def toggle_position_visibility(relayoutData, show_position):
-    if relayoutData and 'position' in relayoutData:
-        return not show_position
-    return show_position
 
 @app.callback(
     Output('query-table-pages', 'data'),
@@ -283,6 +285,9 @@ def update_bar_plot_pages(selected_rows, data):
         selected_data = [data[i] for i in selected_rows]
         df_selected = pd.DataFrame(selected_data)
 
+        df_selected['Clicks'] = pd.to_numeric(df_selected['Clicks'], errors='coerce')
+        df_selected['Impressions'] = pd.to_numeric(df_selected['Impressions'], errors='coerce')
+
         fig = make_subplots(specs=[[{"secondary_y": True}]])
         for column in ['Clicks', 'Impressions']:
             fig.add_trace(
@@ -321,6 +326,9 @@ def update_bar_plot_queries(selected_rows, data):
     if selected_rows:
         selected_data = [data[i] for i in selected_rows]
         df_selected = pd.DataFrame(selected_data)
+
+        df_selected['Clicks'] = pd.to_numeric(df_selected['Clicks'], errors='coerce')
+        df_selected['Impressions'] = pd.to_numeric(df_selected['Impressions'], errors='coerce')
 
         fig = make_subplots(specs=[[{"secondary_y": True}]])
         for column in ['Clicks', 'Impressions']:
@@ -363,10 +371,10 @@ def update_link_pages(selected_rows, data):
     return "#", "Click on a row to visit the page"
 
 @app.callback(
-    Output('bar-plot', 'figure'),
-    [Input('metric-selector', 'value')]
+    Output('bar-plot-device', 'figure'),
+    [Input('metric-selector-device', 'value')]
 )
-def update_bar_graph(selected_metric):
+def update_bar_graph_device(selected_metric):
     return create_bar_figure(selected_metric)
 
 if __name__ == '__main__':
